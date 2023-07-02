1b8a684600898bbb153ef8a860d59bb1e2aef88d
CharlyJoezer <charlyjoezer30@gmail.com>
2023-07-02 22:10:01 +0800

    cra component 2

--- src/Welcome.js
+++ src/Welcome.js
@@ -2,5 +2,12 @@ function Welcome(){
     return <h1>Welcome</h1>
 }
 
+export function WelcomeH2(){
+    return <h2>Welcome H2</h2>
+}
+
+export function WelcomeH3(){
+    return <h3>Welcome H3</h3>
+}
 
 export default Welcome
