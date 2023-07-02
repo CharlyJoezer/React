eae85dcccf74f09f3e75dbd9d89585b9e2475c1b
CharlyJoezer <charlyjoezer30@gmail.com>
2023-07-03 07:41:39 +0800

    static file

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import Button from './Button'
 function App() {
   return (
     <div className="App">
-      <Button>Test button</Button>
+      {/* src wajib menggunakan / diawal */}
+      <img src="/logo512.png" alt='image'/>
+
+      {/* Src salah */}
+      <img src="logo512.png" alt='image'/> 
     </div>
   );
 }
