52f6bf4dcf5a729b018e066220b171087f414498
CharlyJoezer <charlyjoezer30@gmail.com>
2023-07-03 08:36:22 +0800

    Instal react router

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      {/* src wajib menggunakan / diawal */}
-      <img src="/logo512.png" alt='gambar'/>
+      <h1>Halaman Home</h1>
     </div>
   );
 }
